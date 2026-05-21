39fee2d4d5b5b83edaa3c5942d98376c8c340d86
InterRenta <interentamde@gmail.com>
2026-05-21 01:26:19 -0500

    agregar imagenes a vercel

--- interrenta-web/vite.config.js
+++ interrenta-web/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 5000, // en KB; los frames son assets, no JS
+    rollupOptions: {
+      output: {
+        // Mueve los assets de imagen a una carpeta separada
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+  },
+});
